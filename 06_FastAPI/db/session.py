a8250f0b80f56f00d243bcd80c9c2e8f63cf28e3
Elvira Ondar <elvira_kharunova@mail.ru>
2025-08-13 19:18:22 +0300

    added router for create post, updated db session

--- 06_FastAPI/db/session.py
+++ 06_FastAPI/db/session.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from config import DATABASE_URL
+from typing import Generator
 
 # Движок
 engine = create_engine(DATABASE_URL)
@@ -10,5 +11,14 @@ Base = declarative_base()
 
 # Фабрика сессий
 SessionLocal = sessionmaker(bind=engine)
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 
--- 06_FastAPI/routers/post.py
+++ 06_FastAPI/routers/post.py
@@ -1,12 +1,35 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from schemas.post_schema import PostCreate, PostRead
 from db.session import get_db
-from services.post_service import create_post
+from schemas.post_schema import PostCreate, PostRead
+from crud.post_crud import create_post
+from models.tag import Tag
 
-router = APIRouter()
+router = APIRouter(prefix="/posts", tags=["Posts"])
 
-@router.post("/posts/", response_model=PostRead)
+
+@router.post("/", response_model=PostRead)
 def create_post_endpoint(post: PostCreate, db: Session = Depends(get_db)):
-    db_post = create_post(db, post)
-    return db_post  # FastAPI сам преобразует ORM -> PostRead
+    """
+    Эта функция обрабатывает POST-запрос на префикс /posts, когда клиент хочет добавить новый пост.
+
+    Данная функция:
+    1) принимает post (PostCreate) - Pydantic-схему с данными нового поста,
+    2) принимает db (Session) - активную сессию SQLAlchemy,
+    3) проверяет, что введенные теги для поста существуют в базе данных; если нет, вызывает HTTPException,
+    4) вызывает функцию create_post из CRUD, чтобы создать ORM-объект нового поста и сохранить его в базе данных,
+    5) возвращает ответ клиенту.
+
+    Функция возвращает ORM-объект, но FastAPI благодаря response_model преобразует его в Pydantic-схему PostRead,
+    которая содержит данные созданного поста - название, контент, дату, автора, теги, ID поста.
+    """
+    requested_ids = set(post.tags)  # убираем возможные дубликаты ID тегов
+    db_tags = db.query(Tag).filter(Tag.id.in_(requested_ids)).all()  # возвращает список найденных тегов
+
+    if len(db_tags) != len(requested_ids):
+        raise HTTPException(status_code=400, detail="Один или несколько тегов не существуют")
+
+    return create_post(db=db, post=post)
+
+
+
